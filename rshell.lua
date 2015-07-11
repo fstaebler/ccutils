2f32c4d28921f654f8ba14de100751b8c1ed38c6
toplel <sagehttps://github.com/toplel>
2015-07-11 22:37:35 +0200

    wow

--- rshell.lua
+++ rshell.lua
@@ -1,10 +1,16 @@
 print("rshell starting up...")
 --...
-
 if fs.exists("utils") then
+	print("removing old utils...")
 	fs.delete("utils")
+	print("...done.")
 end
-
-
-
+print("installing utils...")
+fs.makeDir("utils")
+programList = [
+	""
+]
+for p in programList do
+	
+end
 print("...done.")
