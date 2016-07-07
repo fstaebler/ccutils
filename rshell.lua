c762a75a11cb274e5fee0b3a515303dc0dd29597
fstaebler <stu116021@mail.uni-kiel.de>
2016-07-07 18:10:57 +0200

    less spam

--- rshell.lua
+++ rshell.lua
@@ -1,23 +1,17 @@
-print("rshell v0.0.1 starting up...")
+print("rshell v0.0.2 starting up...")
 print("initiating remote control loop...")
 result = "BEGIN"
 repeat
   print("Getting a command...")
 	a = http.post("http://kek:8000/result", result)
 	if a then
-		c = a.readAll()
-		print("Received Command:")
-		print(c)
-		print("Executing...")
-		s = loadstring(c)
+		s = loadstring(a.readAll())
     status, result = pcall(s)
 		if not status then
       result = "ERROR\n" .. tostring(result)
-			print("An error occured!")
 		else
       result = "SUCCESS\n" .. tostring(result)
     end
 	end
 until not a
-print("Remote control unavailable!")
 print("Exiting to CraftOS.")
