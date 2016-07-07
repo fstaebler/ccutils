fe1b3df7bbad58cbc7955a614329edd6bd037d87
fstaebler <fle.stae@ymail.com>
2016-07-07 17:27:47 +0200

    rshell does things now. the python part is stoopid tho.

--- rshell.lua
+++ rshell.lua
@@ -13,3 +13,20 @@ for i, p in ipairs(programList) do
 	print("installing ", p, "...")
 end
 print("...done.")
+print("initiating remote control loop...")
+result = "BEGIN"
+repeat
+	a = http.post("http://kek:8000/result", result)
+	if a then
+		c = a.readAll()
+		print("Received Command:")
+		print(c)
+		print("Executing...")
+		s = loadstring(c)
+		if not pcall(s) then
+			print("An error occured!")
+		end
+	end
+until not a
+print("remote control unavailable!")
+print("Exiting to CraftOS.")
---
+++ rslink.py
@@ -0,0 +1,77 @@
+import http.server
+import sqlite3 as sql
+import mimetypes as mimetypes
+import urllib.parse as urlparse
+import json as json
+from datetime import datetime
+
+c = sql.connect("votes.db")
+
+def serve_file(path, s):
+  try:
+    f = open(path, "rb")
+    s.send_response(200)
+
+    #content type
+    ctype = mimetypes.guess_type(path)[0]
+    if None != ctype:
+      s.send_header("Content-type", ctype)
+    else:
+      s.send_header("Content-type", "application/octet-stream") #this will most likely not render and that's good.
+    s.end_headers()
+    #send file contents
+    while True:
+      by = f.read1(1024)
+      if len(by) == 0:
+        break;
+      else:
+        s.wfile.write(by)
+    f.close()
+  except IOError:
+    s.send_error(404)
+
+def handle_command(s):
+	s.send_error(200)
+
+def handle_result(s):
+	print(s.rfile.read())
+	s.send_error(200)
+
+class rslink_handler(http.server.BaseHTTPRequestHandler):
+
+  server_version = "rslink"
+
+  def do_GET(s):
+    path = s.path.split("/")[1:]
+    try:
+      if path[0] == "result": #log client errors
+        handle_result(s)
+      elif path[0] == "command": #receive votes
+        handle_command(s)
+      elif path[0] == "static":
+        serve_file("static/" + path[1], s)
+      elif path[0] == "" or path[0] == "index.html":
+        serve_file("index.html", s)
+      else:
+        s.send_error(403, "Not Allowed", "You are not allowed to access this directory")
+    except:
+      s.send_error(500, "Server Error", "Something the server was not prepared for happened")
+
+	def do_POST(s):
+		path = s.path.split("/")[1:]
+    try:
+      if path[0] == "result": #log client errors
+        handle_result(s)
+      elif path[0] == "command": #receive votes
+        handle_command(s)
+      elif path[0] == "static":
+        serve_file("static/" + path[1], s)
+      elif path[0] == "" or path[0] == "index.html":
+        serve_file("index.html", s)
+      else:
+        s.send_error(403, "Not Allowed", "You are not allowed to access this directory")
+    except:
+      s.send_error(500, "Server Error", "Something the server was not prepared for happened")
+
+server = http.server.HTTPServer(("", 8000), rslink_handler)
+server.serve_forever()
