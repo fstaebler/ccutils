ff640f4ad6b2370effa226d41e70b82e36afd21c
fstaebler <stu116021@mail.uni-kiel.de>
2016-07-08 07:54:38 +0200

    thank you for the pointers

--- rshell.lua
+++ rshell.lua
@@ -1,7 +1,7 @@
 api_endpoint = "http://kek:8000/result"
 
-print("rshell v0.0.5 starting up...")
-result = "BEGIN\n" .. tostring(os.getComputerID) .. "\n" .. tostring(os.getComputerLabel)
+print("rshell v0.0.6 starting up...")
+result = "BEGIN\n" .. tostring(os.getComputerID()) .. "\n" .. tostring(os.getComputerLabel())
 repeat
   print("Getting a command...")
   a = http.post(api_endpoint, result)
@@ -13,7 +13,7 @@ repeat
     else
       statusstring = "SUCCESS\n"
     end
-    result = statusstring .. tostring(os.getComputerID) .. "\n" .. tostring(os.getComputerLabel) .. "\n" .. tostring(result)
+    result = statusstring .. tostring(os.getComputerID()) .. "\n" .. tostring(os.getComputerLabel()) .. "\n" .. tostring(result)
   end
 until not a
 print("Exiting to CraftOS.")
